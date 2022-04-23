3ec79c7de4f61d16d48242d623551daeaab60ce7
Caonabo Mena <cmena7512@gmail.com>
2022-04-22 21:17:07 -0400

    basic structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# F Music
+
+this project is to practice api, ssa moreover it's a gift to someone special  to me.
+i hope you like it!
---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// set public folder as root
+app.use(express.static('public'));
+
+// Allow front-end access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules`));
+
+//Listen for HTTp requests on port 3000
+app.listen(port, () => {
+    console.log('listening on %d', port);
+});
